dd73d419be7c2c0f1b79f4251cb59239b5782358
Israel Wasso <wassoisrael826@gmail.com>
2026-09-03 16:01:06 +0300

    fix: netlify linux build

--- views/intro.js
+++ views/intro.js
@@ -2,5 +2,5 @@ let parent = document.querySelector("#parent")
 parent.classList.add("pulselogo")
 setTimeout(() => {
     window.location.href="accueil.html"
-}, 2000);
+}, 5000);
         //  btn de choix des langues
